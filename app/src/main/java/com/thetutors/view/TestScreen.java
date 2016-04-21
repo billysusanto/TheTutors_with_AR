1f4a5276cc6b29d78bc69c704a7e3caa3c25f2e5
billysusanto <billysusantop@gmail.com>
2016-04-21 13:01:45 +0700

    Change TestScreen content from string.xml to Servlet
    *not Tested yet

--- app/src/main/java/com/thetutors/view/TestScreen.java
+++ app/src/main/java/com/thetutors/view/TestScreen.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -17,31 +17,52 @@ import android.widget.TextView;
 
 import com.thetutors.R;
 import com.thetutors.controller.VariabelConfig;
+import com.thetutors.model.QuestionTest;
+import com.thetutors.webservice.JavaServlet;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import static android.view.ViewGroup.MarginLayoutParams;
 
-public class TestScreen extends ActionBarActivity {
+public class TestScreen extends AppCompatActivity {
 
     LinearLayout mainLayout;
     int max = 10;
 
-    List<String> list_question = new LinkedList<String>();
-    List<String> list_answer = new LinkedList<String>();
-    List<String> list_multi_choice = new LinkedList<String>();
+    //List<String> list_question = new LinkedList<String>();
+    //List<String> list_answer = new LinkedList<String>();
+    //List<String> list_multi_choice = new LinkedList<String>();
     List<Integer> sequence = new LinkedList<Integer>();
 
     boolean checker [];
 
     Random random = new Random();
-    boolean getQuestion = false;
+    //boolean getQuestion = false;
     RadioGroup rg [];
     VariabelConfig publicVar;
     Context context;
+
+    QuestionTest questionTest [];
+    JavaServlet servlet = new JavaServlet();
+    String msg;
+    String [] split_multi_choice;
 
     int temp [] = new int [max];
     @Override
@@ -65,10 +86,20 @@ public class TestScreen extends ActionBarActivity {
         Button finish = new Button(this);
         checker = new boolean [Arrays.asList(getResources().getStringArray(R.array.question)).size()];
 
+        try {
+            msg = servlet.execute(3, 1).get();
+            questionTest = xmlParser(msg);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         //get content from string.xml (question, answer, multiplechoice)
-        list_question = Arrays.asList(getResources().getStringArray(R.array.question));
-        list_answer = Arrays.asList(getResources().getStringArray(R.array.answer));
-        list_multi_choice = Arrays.asList(getResources().getStringArray(R.array.multi_choice));
+
+//        list_question = Arrays.asList(getResources().getStringArray(R.array.question));
+//        list_answer = Arrays.asList(getResources().getStringArray(R.array.answer));
+//        list_multi_choice = Arrays.asList(getResources().getStringArray(R.array.multi_choice));
 
         mainLayout = (LinearLayout) findViewById(R.id.questionLL);
         mainLayout.setOrientation(LinearLayout.VERTICAL);
@@ -106,7 +137,8 @@ public class TestScreen extends ActionBarActivity {
 
             //Check is the question is already show on the screen yet.
             while(true) {
-                temp[i] = random.nextInt(Arrays.asList(getResources().getStringArray(R.array.question)).size()-1);
+                //temp[i] = random.nextInt(Arrays.asList(getResources().getStringArray(R.array.question)).size()-1);
+                temp[i] = random.nextInt(questionTest.length-1);
                 if(checker[temp[i]] == false){
                     checker[temp[i]] = true;
                     break;
@@ -125,13 +157,17 @@ public class TestScreen extends ActionBarActivity {
             lla[i].addView(rg[i]);
 
             //set the value of question and answer
-            number[i].setText(i+1+". ");
-            String split_multi_choice [] = list_multi_choice.get(temp[i]).split("#");
-            question[i].setText(list_question.get(temp[i]));
-            a[i].setText("a. "+split_multi_choice[0]);
-            b[i].setText("b. "+split_multi_choice[1]);
-            c[i].setText("c. "+split_multi_choice[2]);
-            sequence.add(Integer.parseInt(list_answer.get(temp[i])));
+            number[i].setText(questionTest[i].getId());
+            //String split_multi_choice [] = list_multi_choice.get(temp[i]).split("#");
+            question[i].setText(questionTest[i].getQuestion());
+            split_multi_choice = questionTest[i].getMultipleChoice();
+
+            for(int j=0; j<split_multi_choice.length; j++) {
+                a[i].setText("a. " + split_multi_choice[0]);
+                b[i].setText("b. " + split_multi_choice[1]);
+                c[i].setText("c. " + split_multi_choice[2]);
+            }
+            sequence.add(Integer.parseInt(questionTest[i].getAnswer()));
         }
 
         //add finish button to layout
@@ -149,7 +185,7 @@ public class TestScreen extends ActionBarActivity {
 
                     //algorithm to check the result
                     if(idx != -1) {
-                        String user_answer_check[] = list_multi_choice.get(temp[i]).split("#");
+                        String user_answer_check[] = split_multi_choice;
                         if (Integer.parseInt(user_answer_check[idx]) == sequence.get(i)) {
                             publicVar.incResult();
                         }
@@ -196,5 +232,61 @@ public class TestScreen extends ActionBarActivity {
             return super.onKeyDown(keyCode, event);
         }
 
+    }
+
+    public QuestionTest[] xmlParser(String xml){
+        QuestionTest questionTest [] = new QuestionTest[0];
+
+        try{
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+            //replace String as InputSource
+            InputSource is = new InputSource(new StringReader(xml));
+            Document doc = dBuilder.parse(is);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("no");
+
+            questionTest = new QuestionTest[nList.getLength()];
+
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node nNode = nList.item(i);
+                Element e = (Element) nNode;
+                questionTest[i] = new QuestionTest();
+
+                questionTest[i].setId(Integer.parseInt(nNode.getAttributes().getNamedItem("id").getNodeValue()));
+
+                NodeList question = e.getElementsByTagName("question");
+                NodeList multipleChoice = e.getElementsByTagName("multiple-choice");
+                NodeList answer = e.getElementsByTagName("answer");
+
+                for(int j=0; j<question.getLength(); j++){
+                    questionTest[i].setQuestion(question.item(j).getTextContent());
+                }
+
+                String multipleChoiceTemp [] = new String [multipleChoice.getLength()];
+                for(int j=0; j<multipleChoice.getLength(); j++){
+                    multipleChoiceTemp[i] = multipleChoice.item(j).getTextContent();
+                }
+                questionTest[i].setMultipleChoice(multipleChoiceTemp);
+
+                for(int j=0; j<answer.getLength(); j++){
+                    questionTest[i].setAnswer(answer.item(j).getTextContent());
+                }
+            }
+        }
+        catch(ParserConfigurationException e){
+            Log.e("ParseConfigExc", e.toString());
+        }
+        catch(IOException e){
+            Log.e("IOException", e.toString());
+        }
+        catch(SAXException e){
+            Log.e("SAXException", e.toString());
+        }
+
+        return questionTest;
     }
 }
